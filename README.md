56fedcc45f393cb611c8906cc3c083be097e2ecd
oscar villagrana <>
2019-11-30 13:21:13 -0800

    v.02

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # calendar-web-scraper
 create a database from calendar websites
 
-## Contributors
-
-[//]
-
-<a href="https://github.com/eagarcia8"><img src="https://avatars.githubusercontent.com/eagarcia8.png?size=200" title="eagarcia8" width="80" height="80"></a>
-
-[//]
--- calendar-scraper.py
+++ calendar-scraper.py
@@ -12,18 +12,30 @@ headers = {'User-Agent':'Mozilla/5.0'}
 def get_soup():
     session = requests.Session()
     pageTree = session.get(page, headers=headers)
-    return BeautifulSoup(pageTree.content, 'html.parser')
+    return BeautifulSoup(pageTree.content, 'html5lib')
 
 pageSoup = get_soup()
 
-print(pageSoup)
+# print(pageSoup.pritify())
+
+    """
+    TODO: need to make a parser because page is badly formed
+
+    html = urllib2.urlopen(page).read()
+    for para in html.split("</p>"):
+        if '<p class="lead">' in para:
+            abstract=para.split('<p class="lead">')[1:][0]
+            print ' '.join(abstract.split("\n"))
+
+    """
 
 
+"""
 # the sections I am scraping
 section.date.dtstart
 a.event-url
 
-# gets dict
+# gets dict[//]:
 pageSoup.a.attrs
 
 # gets list of strings
@@ -31,3 +43,4 @@ pageSoup.a['class']
 
 # also gets list of strings
 pageSoup.a.get_attribute_list('class')   
+"""
